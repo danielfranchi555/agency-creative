67df7c48065a421a44e2c4d501599f0080a66834
danielfranchi555 <danielfranchi3@gmail.com>
2024-01-01 16:10:13 -0300

    colors

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
+import Hero from '@/components/hero/Hero'
+import Navbar from '@/components/navbar/Navbar'
 import Image from 'next/image'
 
 export default function Home() {
   return (
   <>
-   <h1 className='bg-red-500' >hola</h1>
+  <Navbar/>
+  <Hero/>
   </>
   )
 }
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-green-500'>
+       navbar 
+    </div>
+  )
+}
+
+export default Navbar
